72fd57fb0460575cf9c47cfac0b974261771ee59
rosebellotaza <rosebellnarieotaza@gmail.com>
2025-12-02 02:48:54 +0800

    updates

--- js/index.js
+++ js/index.js
@@ -9,48 +9,4 @@ $(document).ready(function() {
 });
 
 
-///// More Animations in here pamili unsay bet
-/*1
-function slideUpSplashScreen() {
-    $(".se-pre-con").slideUp(600, function() {
-        $("body").css("background", "linear-gradient(to right, #FFBB5C, #C63D2F)");
-        window.location.href = "signin.html";
-    });
-}
-
-
-$(document).ready(function() {
-    setTimeout(slideUpSplashScreen, 3000);
-});
-*/
-
-/*2
-function slideLeftSplashScreen() {
-    $(".se-pre-con").animate({
-        "left": "-100%"
-    }, 600, function() {
-        $("body").css("background", "linear-gradient(to right, #FFBB5C, #C63D2F)");
-        window.location.href = "signin.html";
-    });
-}
-
-$(document).ready(function() {
-    setTimeout(slideLeftSplashScreen, 3000);
-});
-*/
-
-/*3
-function slideRightSplashScreen() {
-    $(".se-pre-con").animate({
-        "left": "100%"
-    }, 600, function() {
-        $("body").css("background", "linear-gradient(to right, #FFBB5C, #C63D2F)");
-        window.location.href = "signin.html";
-    });
-}
-
-$(document).ready(function() {
-    setTimeout(slideRightSplashScreen, 3000);
-});
-*/
 
